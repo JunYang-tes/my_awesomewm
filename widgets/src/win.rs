4869f0fdd21e8a747de877f6d5d88b41d2697f2d
YJ <jun.yang@target-energysolutions.com>
2023-12-06 16:51:02 +0800

    start x11 event loop and draw stuffs on window

--- widgets/src/win.rs
+++ widgets/src/win.rs
@@ -1,3 +1,6 @@
+use std::sync::Arc;
+
+use cairo::*;
 use mlua::prelude::*;
 use once_cell::sync::Lazy;
 use xcb::x;
@@ -5,22 +8,94 @@ struct Win {
     window: xcb::x::Window,
 }
 struct Context {
-    connection: xcb::Connection,
+    connection: std::sync::Arc<xcb::Connection>,
     screen_num: i32,
+    event_loop: std::thread::JoinHandle<()>,
 }
 
 impl Context {
     fn new() -> Context {
         let (conn, screen_num) = xcb::Connection::connect(None).unwrap();
+        let connection = Arc::new(conn);
+        let conn = connection.clone();
         println!("create context");
+
+        let event_loop = std::thread::spawn(move || {
+            println!("Event loop started");
+            let setup = conn.get_setup();
+            let screen = setup.roots().nth(screen_num as usize).unwrap();
+            loop {
+                let event = match conn.wait_for_event() {
+                    Ok(event) => event,
+                    Err(_) => {
+                        return;
+                    }
+                };
+                match event {
+                    xcb::Event::X(x::Event::Expose(e)) => {
+                        let visual = find_visual(&conn, screen.root_visual()).unwrap();
+                        unsafe {
+                            let raw_conn = conn.get_raw_conn();
+                            let xcb_conn =
+                                XCBConnection::from_raw_full(std::mem::transmute(raw_conn));
+                            let drawable = XCBDrawable(xcb::Xid::resource_id(&e.window()));
+                            let xcb_visual =
+                                XCBVisualType::from_raw_full(std::mem::transmute(&visual));
+                            draw_on_win(&xcb_conn, &drawable, &xcb_visual, 150, 150).unwrap();
+                            conn.flush().unwrap();
+                        }
+                        println!("Expose")
+                    }
+                    _ => {}
+                }
+            }
+        });
         Context {
-            connection: conn,
+            connection,
             screen_num,
+            event_loop
         }
     }
 }
 
 static CONTEXT: Lazy<Context> = Lazy::new(|| Context::new());
+
+fn find_visual<'a>(
+    conn: &'a xcb::Connection,
+    visual: xcb::x::Visualid,
+) -> Option<xcb::x::Visualtype> {
+    let setup = conn.get_setup();
+    for screen in setup.roots() {
+        let d_iter = screen.allowed_depths();
+        for depth in d_iter {
+            for vis in depth.visuals() {
+                if visual == vis.visual_id() {
+                    println!("Found visual");
+                    return Some((*vis).clone());
+                }
+            }
+        }
+    }
+    None
+}
+fn draw_on_win(
+    conn: &XCBConnection,
+    drawable: &XCBDrawable,
+    visual: &XCBVisualType,
+    width: i32,
+    height: i32,
+) -> Result<()> {
+    let surface = cairo::XCBSurface::create(conn, drawable, visual, width, height)?;
+    let cr = cairo::Context::new(&surface)?;
+    cr.set_source_rgb(0.0, 1.0, 0.0);
+    cr.move_to(0.0, 0.0);
+    cr.line_to(100.0, 100.0);
+    cr.stroke()?;
+    surface.flush();
+    //cr.paint()?;
+    println!("DONE");
+    Ok(())
+}
 
 impl LuaUserData for Win {
     fn add_fields<'lua, F: LuaUserDataFields<'lua, Self>>(fields: &mut F) {
